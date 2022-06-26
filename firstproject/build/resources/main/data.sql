4924afa389057fd3447a2b7e307319c91604e3aa
Cha Hyoung Seok <xcohdsuts@gmail.com>
2022-06-27 02:04:57 +0900

    Delete firstproject directory

--- firstproject/build/resources/main/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO article(id, title, content) values (1, 'aaa', '111')
-INSERT INTO article(id, title, content) values (2, 'bbb', '222')
-INSERT INTO article(id, title, content) values (3, 'ccc', '333')
